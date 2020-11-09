b4220012fa5e0eca084f2cb6e35a5ab1a4483a4d
Arne Johannes Holmin <arnejh@hi.no>
2020-11-09 11:34:26 +0100

    Added R/RstoxBuild-package.R

---
+++ R/RstoxBuild-package.R
@@ -0,0 +1,10 @@
+#' Package for Building All Rstox Packages
+#'
+#' Building the Rstox packages (Rstox, RstoxFramework, RstoxData, RstoxFDA, RstoxSurveyPlanner, RstoxTempdoc, and even RstoxBuild), and semi-automated testing of Rstox though test projects.
+#'
+#' The package defines titles, descriptions, dependencies, authors, install instructions and other info for all the packages. All changes to authors, descriptions, suggests and other outputs of the function \code{packageSpecs} should be changed in this package, and not in the individual packages. The package also contains functionality for semi-automated testing of Rstox on a set of test projects.
+#' @docType package
+#' @name RstoxBuild
+#'
+"_PACKAGE"
+
